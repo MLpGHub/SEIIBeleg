161451157b7a355142127e2cf2469264d4c017a1
s76954 <s76954@informatik.htw-dresden.de>
2019-05-11 22:16:41 +0200

    Testklasse hinzugefügt

---
+++ Projektordner Java/TicTacToe/src/test/java/TicTacToe/TicTacToeTests.java
@@ -0,0 +1,31 @@
+package TicTacToe;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TicTacToeTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void driverTest() {
+		Driver d = new Driver();
+	}
+	
+	//Verhalten??
+	//-> Eventhandler?
+	
+	//Driver: shows
+	
+	//InfoScreen: paints
+	
+	//Menu: paints
+	
+	//Sieg: setSieger, paints
+	
+	//Spiel: clearField, validateField, paint
+
+}
