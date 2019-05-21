b1d4b058da0e29e619c75356285c4c5eadfd4a87
Emanuel Guenther <s76954@smb.informatik.htw-dresden.de>
2019-05-21 11:02:40 +0200

    .bat-Skripte für Ausführung und Kopieren hinzugefügt

--- copy_final.bat
+++ copy_final.bat
@@ -1,20 +1,27 @@
-#clean up final dir
-rm final/*.* 2> /dev/null
+del /s /f /Q final\*.*
+rd /s /f final\*
+dir final\
 
-#copy all files and artefacts
-cp Dokumente\ AG/Ausschreibung/ausschreibung.pdf final/Ausschreibung
-cp Dokumente\ AG/Firmenportraet/firmenportraet.pdf final/Firmenportraet_AG.pdf
-cp Dokumente\ AG/Lastenheft/lastenheft.pdf final/Lastenheft.pdf
-cp Dokumentenordner/Angebot/angebot.pdf final/Angebot.pdf
-cp Dokumentenordner/Entwicklungsumgebung/Entwicklungsumgebung.pdf final
-cp Dokumentenordner/Firmenportr√§t/firmenportraet.pdf final/Firmenportraet_AN.pdf
-cp Dokumentenordner/Kalkulation/Kostenaufstellung_extern.pdf final
-cp Dokumentenordner/Kalkulation/Kostenaufstellung_intern.pdf final
-cp Dokumentenordner/Kalkulation/Kalkulation_intern.pdf final
-cp Dokumentenordner/Pflichtenheft/pflichtenheft.pdf final/Pflichtenheft.pdf
-cp Dokumentenordner/projektbegleitender\ Bericht/bericht.pdf final/Projektbegleitender_Bericht.pdf
-cp Projektordner\ Java/TicTacToe/dist/TicTacToe_pg.jar final/TicTacToe.jar
-cp -r Projektordner\ Java/TicTacToe/doc final/JavaDoc
-cp -r Projektordner\ Prototyp/export final/Prototyp
-cp Projektordner\ UML/*.pdf final
-
+xcopy "Dokumente AG\Ausschreibung\ausschreibung.pdf" final\
+ren final\ausschreibung.pdf Ausschreibung.pdf
+xcopy "Dokumente AG\Firmenportraet\firmenportraet.pdf" final\
+ren final\firmenportraet.pdf Firmenportraet_AG.pdf
+xcopy "Dokumente AG\Lastenheft\lastenheft.pdf" final\
+ren final\lastenheft.pdf Lastenheft.pdf
+xcopy "Dokumentenordner\Angebot\angebot.pdf" final\
+ren final\angebot.pdf Angebot.pdf
+xcopy "Dokumentenordner\Entwicklungsumgebung\Entwicklungsumgebung.pdf" final\
+xcopy "Dokumentenordner\Firmenportr‰t\firmenportraet.pdf" final\
+ren final\firmenportraet.pdf Firmenportraet_AN.pdf
+xcopy "Dokumentenordner\Kalkulation\Kostenaufstellung_extern.pdf" final\
+xcopy "Dokumentenordner\Kalkulation\Kostenaufstellung_intern.pdf" final\
+xcopy "Dokumentenordner\Kalkulation\Kalkulation_intern.pdf" final\
+xcopy "Dokumentenordner\Pflichtenheft\pflichtenheft.pdf" final\
+ren final\pflichtenheft.pdf Pflichtenheft.pdf
+xcopy "Dokumentenordner\projektbegleitender Bericht\bericht.pdf" final\
+ren final\bericht.pdf Projektbegleitender_Bericht.pdf
+xcopy "Projektordner Java\TicTacToe\dist\TicTacToe_pg.jar" final\
+ren final\TicTacToe_pg.jar TicTacToe.jar
+xcopy /s "Projektordner Java\TicTacToe\doc" final\JavaDoc
+xcopy /s "Projektordner Prototyp\export" final\Prototyp
+xcopy "Projektordner UML\*.pdf" final\
