f5f1386d0f94a72b4a48a4b2edc93254b36f7fd7
s76954 <s76954@informatik.htw-dresden.de>
2019-06-11 17:43:50 +0200

    JUnit-Test korrigiert

--- Projektordner Java/TicTacToe/tests/core/TicTacToeTests.java
+++ Projektordner Java/TicTacToe/tests/core/TicTacToeTests.java
@@ -1,12 +1,18 @@
 package core;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferInt;
+import java.io.File;
+import java.nio.ByteBuffer;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageInputStream;
 import core.Driver;
 import core.InfoScreen;
 import core.Sieg;
@@ -29,7 +35,7 @@ public class TicTacToeTests {
 
   @Test
   public void infoScreenTest() {
-    Image i = Toolkit.getDefaultToolkit().getImage("src/main/resources/regeln.jpg");
+    Image i = Toolkit.getDefaultToolkit().getImage("src/core/resources/regeln.jpg");
     InfoScreen is = new InfoScreen(new Driver(), i);
     assert (is.background != null);
   }
@@ -55,19 +61,31 @@ public class TicTacToeTests {
   }
 
   @Test
+  //@Ignore
   public void aboutTest() {
-    BufferedImage ai = (BufferedImage)Toolkit.getDefaultToolkit().getImage("src/main/resources/about.jpg");
+    BufferedImage ai = null;
+    try {
+      //System.out.println("Working Directory = " + System.getProperty("user.dir"));
+      ai = ImageIO.read(new FileImageInputStream(new File("../resources/about.jpg")));
+    } catch (Exception e) {
+      System.out.println(e);
+    }
     InfoScreen about = new InfoScreen(new Driver(), ai);
     Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
     BufferedImage testImg = new BufferedImage(d.width, d.height, BufferedImage.TYPE_INT_ARGB);
     Graphics testg = testImg.getGraphics();
-    int[] refData = ((DataBufferInt)ai.getData().getDataBuffer()).getData();
+    int[] refData = new int[(ByteBuffer.wrap(((DataBufferByte)ai.getData().getDataBuffer()).getData()).asIntBuffer()).remaining()];
     int[] testData;
     
     about.paint(testg);
     testData = ((DataBufferInt)testImg.getData().getDataBuffer()).getData();
     
+    //System.out.println("len(rD)=" + refData.length + ", len(tD)=" + testData.length);
+    
     for (int i = 0; i < refData.length; i++) {
+      //System.out.println("a[i]=" + a[i] + ", tD[i]=" + testData[i]);
+      //System.out.println("rD[i] = " + String.format("0x%08X", refData[i]) 
+      //  + ", tD[i] = " + testData[i]);
       assert(refData[i] == testData[i]);
     }
   }
