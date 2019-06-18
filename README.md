46e6c6d6e2bc42bb859d0b49aebcb10c1a52c272
s76954 <s76954@informatik.htw-dresden.de>
2019-06-18 12:55:00 +0200

    Aktualisierungen in Copy-Skripten und README

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Beleg Software Engineering II
 
 ## Inhalt
@@ -12,39 +11,10 @@
 
 ## tmp notes for doc
 * 29.5., 1.5h, About-Grafik, Update Pflichtenheft, Aktualisierung UML
-
-## Dokumentation git Updates
-
-* falls immer **derselbe lokale Ordner** für das Repo genutzt wird, aktuelle Daten holen mit `$ git pull <url>`
-
-* falls ein **temporärer Ordner** für das Repo genutzt wird, aktuelle Daten holen mit `$ git clone <url>`
-
-  * Begründung: bei git pull muss ein bereits initialisiertes Repo vorhanden sein
-  * macht man dieses init manuell und dann `$ git pull`, kommen die Branches durcheinander
-  * deshalb bei temporären lokalen Kopien mit `$ git clone` initialisieren
-  * über `$ git checkout <branch>` sind dann auch die remote-Branches verfügbar
-
-
-## Infos zu Markdown
-
-* https://daringfireball.net/projects/markdown/syntax
-* https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
-
-=======
-# Beleg Software Engineering II
-
-## Inhalt
-
-* Repo für den SE - Beleg
-* Mitglieder der Gruppe:
-  * Michael Leopold
-  * Emanuel Günther
-  * Robert Siedler
-  * Patrick Creutzburg
-
-## Notizen projbegl. Bericht
 * Emanuel, 11.6., 1h; JUnit Krams
 * alle, 12.6., 1.5h, Praktikum; Präsentation
+* Emanuel/Micha, 18.6., 2h, Merges und UPDATE
+
 
 ## Dokumentation git Updates
 
@@ -62,5 +32,3 @@
 
 * https://daringfireball.net/projects/markdown/syntax
 * https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
-
->>>>>>> doc
--- copy_final.bat
+++ copy_final.bat
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-del /s /f /Q final\*.*
-rd /s /f final\*
-dir final\
-
-xcopy "Dokumente AG\Ausschreibung\ausschreibung.pdf" final\
-ren final\ausschreibung.pdf Ausschreibung.pdf
-xcopy "Dokumente AG\Firmenportraet\firmenportraet.pdf" final\
-ren final\firmenportraet.pdf Firmenportraet_AG.pdf
-xcopy "Dokumente AG\Lastenheft\lastenheft.pdf" final\
-ren final\lastenheft.pdf Lastenheft.pdf
-xcopy "Dokumentenordner\Angebot\angebot.pdf" final\
-ren final\angebot.pdf Angebot.pdf
-xcopy "Dokumentenordner\Anwenderdoku\Anwenderdoku.pdf" final\
-ren final\Anwenderdoku.pdf Anwenderdokumentation.pdf
-xcopy "Dokumentenordner\Entwicklungsumgebung\Entwicklungsumgebung.pdf" final\
-xcopy "Dokumentenordner\Firmenportraet\firmenportraet.pdf" final\
-ren final\firmenportraet.pdf Firmenportraet_AN.pdf
-xcopy "Dokumentenordner\Kalkulation\Kostenaufstellung_extern.pdf" final\
-xcopy "Dokumentenordner\Kalkulation\Kostenaufstellung_intern.pdf" final\
-xcopy "Dokumentenordner\Kalkulation\Kalkulation_intern.pdf" final\
-xcopy "Dokumentenordner\Pflichtenheft\pflichtenheft.pdf" final\
-ren final\pflichtenheft.pdf Pflichtenheft.pdf
-xcopy "Dokumentenordner\projektbegleitender Bericht\bericht.pdf" final\
-ren final\bericht.pdf Projektbegleitender_Bericht.pdf
-xcopy "Projektordner Java\TicTacToe\dist\TicTacToe_pg.jar" final\
-ren final\TicTacToe_pg.jar TicTacToe.jar
-xcopy /s "Projektordner Java\TicTacToe\dist\resources" final\resources
-xcopy /s "Projektordner Java\TicTacToe\doc" final\JavaDoc
-xcopy /s "Projektordner Prototyp\export" final\Prototyp
-xcopy "Projektordner UML\*.pdf" final\UML
-=======
 del /s /f /Q final\*.*
 rd /s /f final\*
 dir final\
